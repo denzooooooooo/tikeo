38669eae0f420e54851dee842ab8d0065935f045
Ange Djedjed <djedjedange20@gmail.com>
2026-03-06 21:52:43 +0100

    nijk

--- services/api-gateway/src/admin/admin.module.ts
+++ services/api-gateway/src/admin/admin.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
+import { PrismaModule } from '../prisma/prisma.module';
 import { EmailModule } from '../email/email.module';
 
 @Module({
-  imports: [EmailModule],
+  imports: [PrismaModule, EmailModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
