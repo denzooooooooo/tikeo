417d552a11049b4d620320a6d7734e335e70e11d
Ange Djedjed <djedjedange20@gmail.com>
2026-03-10 02:29:48 +0100

    nxds

---
+++ packages/ui/tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+      },
+      borderColor: {
+        border: 'hsl(var(--border))',
+      },
+      fontFamily: {},
+      fontSize: {},
+    },
+  },
+  plugins: [],
+};
+
+export default config;
