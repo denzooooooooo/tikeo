b1ebc029e0ff18376c8bcfcc922b1750b2a0cf6b
Ange Djedjed <djedjedange20@gmail.com>
2026-03-10 14:51:10 +0100

    layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -137,7 +137,12 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Providers>
-          {children}
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-1">{children}</main>
+            <GlassFooter />
+            <CookieConsentBanner />
+          </div>
         </Providers>
       </body>
     </html>
